7a46b193df39cf7bf2d35f11f705c434ea413037
madel-04 <maydvg04@gmail.com>
2024-09-19 19:49:33 +0200

    dia19_4

--- ft_itoa.c
+++ ft_itoa.c
@@ -14,9 +14,45 @@
 
 /*Usa malloc y genera una string que represente el valor 
 entero recibido como argumento. Gestiona NEGAATIVOS*/
+static int	ft_num_len(int n) /*Static para qie solo sea visible para el archivo donde se define*/
+{
+	int	len;
+
+	len = 1; /*Posible numero negativo*/
+	if (n < 0)
+		len++;
+	while (n / 10 != 0) /*600 / 10 = 60 / 10 = 6*/
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
+	int	len;
+	long	nb;
 
-
+	nb = n; /*para gestionar los valores mejor*/
+	len = ft_num_len(nb);
+	str = (char *)malloc(sizeof(char) * (len +1));
+	if (!str)
+		return (NULL);
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (nb > 9)
+	{
+		str[--len] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	str[--len] = nb + '0';
+	return (str);
 }
+/*La diferencia entre poner --len y len-- es que en la primera
+decrementa el valor antes de usar la variable y en la segunda
+usa el valor de la variable y luego lo decrementa*/
---
+++ ft_strmapi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 19:39:33 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/19 19:39:33 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+/*Puntero a una función que retorna un char y que de parametros tiene un unsigned int y char*/
+{
+	char	*dest;
+	size_t	len; /*Evitar desbordamientos*/
+	size_t	i;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dest[i] = f(i, s[i]);
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
